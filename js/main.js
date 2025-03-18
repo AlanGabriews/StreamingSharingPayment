e3e49273e74a1818d0f60cc457cb3215dd9b49e6
AlanGabriews <ms.alangabriel@gmail.com>
2025-03-18 17:57:05 -0300

    project structure and landing page(unfinished)
    A start to organizing the project better and a very basic(and very unfinished) landing page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const today = new Date();
+const dueDateM = new Date(today.getFullYear(), today.getMonth(), 1);
+const dueDateA = new Date(today.getFullYear(), 0, 1);
+
 const users = [
     {name: "Alan", phone: "+5531994144202", email: "ms.alangabriel@gmail.com"},
     {name: "Emilly", phone: "+5531912345678", email: "emilly@gmail.com"},
@@ -13,6 +17,24 @@ const subscriptions = [
     {service: "DisneyPlus", cost: 61.48, frequency: "annual"},
     {service: "DisneyPlusPremium", cost: 93.28, frequency: "annual"},
 ];
+
+function isDueDateM()
+{
+    return (
+        today.getDate() === dueDateM.getDate() &&
+        today.getMonth() === dueDateM.getMonth() &&
+        today.getFullYear() === dueDateM.getFullYear()
+        );
+}
+
+function isDueDateA()
+{
+    return (
+        today.getDate() === dueDateA.getDate() &&
+        today.getMonth() === dueDateA.getMonth() &&
+        today.getFullYear() === dueDateA.getFullYear()
+        );
+}
 
 function sendReminder(user, subscription)
 {
