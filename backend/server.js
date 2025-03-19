7f2ccca760d3b45dbdace062be95ee6011b8bf13
AlanGabriews <ms.alangabriel@gmail.com>
2025-03-19 12:00:06 -0300

    backend server

--- backend/server.js
+++ backend/server.js
@@ -23,11 +23,55 @@ app.post("/signup", (req, res) => {
         "SELECT * FROM users WHERE username = ?",
         [username],
         (error, results) => {
-            if(error) {
+            if(error)
+            {
                 return res.status(400).json({ error: "Username already exists" });
             }
         
-            
+            if(results.length > 0)
+            {
+                return res.status(400).json({ error: "username already exists" });
+            }
+
+            pool.query(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                [username, password],
+                (error, results) => {
+                    if(error)
+                    {
+                        return res.status(500).json({ error: "Database error"});
+                    }
+                    res.status(201).json({ message: "Signup successful"});
+                }
+            );
         }
-    )
-})
+    );
+});
+
+app.post("/login", (req, res) => {
+    const {username, password} = req.body;
+
+    pool.query(
+        "SELECT * FROM users WHERE username = ? AND password = ?",
+        [username, password],
+        (error, results) => {
+            if(error)
+            {
+                return res.status(500).json({ error: "Database error" });
+            }
+            
+            if(results.length === 0)
+            {
+                return res.status(401).json({ error: "Invalid username or password" });
+            }
+
+            const user = { id: results[0].id, username: results[0].username };
+            res.status(200).json({ message: "Login successful", user});
+        }
+    );
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
